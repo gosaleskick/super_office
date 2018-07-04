97d1631c1adad3ad6feca421c8477347241c08a8
DiodonHystrix <diodonhystrix7@gmail.com>
2018-07-04 15:27:35 +0200

    Add configuration to SuperOffice module

--- lib/super_office.rb
+++ lib/super_office.rb
@@ -1,5 +1,22 @@
+# frozen_string_literal: true
+
 require "super_office/version"
+require 'typhoeus'
+require 'oj'
+require 'jwt'
+require 'rbnacl/libsodium'
+
+require 'super_office/configuration'
+require 'super_office/encryptor'
 
 module SuperOffice
-  # Your code goes here...
+  class << self
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def configure
+      yield configuration
+    end
+  end
 end
