9d85f81a3edd3284c06d4e96d976cd3c4132ec97
DiodonHystrix <diodonhystrix7@gmail.com>
2018-07-04 14:12:18 +0200

    Setup rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 require "bundler/setup"
 require "super_office"
+require 'support/token_ar'
+require 'support/vcr'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -7,8 +11,19 @@ RSpec.configure do |config|
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
+  config.expose_dsl_globally = true
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.before(:all) do
+    SuperOffice.configure do |config|
+      config.client_id = 'client_id'
+      config.client_secret = 'client_secret'
+      config.redirect_uri = 'test_redirect_uri'
+      config.secret_encryption_key = "\xB2\x9Dx\xF8\x12\x8B\xEF\xD9\xFA\x94\x8D\x1C\xCA \xEE\"xo,\xBEn\x93)\xB2\xFD\xDE\x87cA\xF9j\a"
+      config.environment = :development
+    end
   end
 end
---
+++ spec/support/vcr.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+require 'vcr'
+
+VCR.configure do |c|
+  c.default_cassette_options = { match_requests_on: [:method, :uri, :body] }
+  c.cassette_library_dir = 'spec/vcr'
+  c.hook_into :typhoeus
+  c.configure_rspec_metadata!
+end
