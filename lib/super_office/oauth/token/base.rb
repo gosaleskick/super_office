4064e9dd2e6ee643bbc3ea25d7c514935fba52b5
DiodonHystrix <diodonhystrix7@gmail.com>
2018-07-04 15:16:10 +0200

    Add OAuth::Token Fetcher and Refresher classes

---
+++ lib/super_office/oauth/token/base.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+module SuperOffice
+  module OAuth
+    module Token
+      class Base
+        JWT_API_ENDPOINT_KEY = 'http://schemes.superoffice.net/identity/webapi_url'
+
+        private
+
+        def token_attributes
+          {
+            api_endpoint: api_endpoint,
+            encrypted_access_token: SuperOffice::Encryptor.encrypt(response_body[:access_token]),
+            encrypted_refresh_token: encrypted_refresh_token,
+            expires_at: expires_at
+          }
+        end
+
+        def expires_at
+          @expires_at ||= Time.parse(response.headers['date']) + response_body[:expires_in].to_i
+        end
+
+        def api_endpoint
+          JWT.decode(response_body[:id_token], nil, false).first[JWT_API_ENDPOINT_KEY]
+        end
+
+        def encrypted_refresh_token
+          SuperOffice::Encryptor.encrypt(response_body[:refresh_token])
+        end
+
+        def response
+          raise NotImplementedError
+        end
+
+        def post_request(grant_type, params)
+          Typhoeus::Request.post(
+            token_request_url,
+            body: {
+              grant_type: grant_type,
+              client_id: SuperOffice.configuration.client_id,
+              client_secret: SuperOffice.configuration.client_secret,
+              redirect_uri: SuperOffice.configuration.redirect_uri,
+              scope: :openid
+            }.merge!(params),
+            headers: {
+              'Content-Type' => 'application/x-www-form-urlencoded'
+            }
+          )
+        end
+
+        def token_request_url
+          "https://#{SuperOffice.configuration.subdomain}.superoffice.com/login/common/oauth/tokens"
+        end
+
+        def response_body
+          @response_body ||= Oj.load(response.response_body, symbol_keys: true)
+        end
+      end
+    end
+  end
+end
---
+++ spec/super_office/oauth/token/refresher_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe SuperOffice::OAuth::Token::Refresher do
+  let(:token) { TokenAR.new(expires_at: Date.parse('2018-01-01').to_time) }
+
+  describe '#refresh' do
+    context 'successful refresh' do
+      it 'returns updated token', :vcr do
+        updated_token = described_class.new(token).refresh
+
+        expect(updated_token.api_endpoint).to eq('https://sod.superoffice.com/Cust25708/api/')
+        expect(updated_token.expires_at).to eq(Time.parse('Wed, 04 Jul 2018 12:59:34 GMT'))
+        expect(SuperOffice::Encryptor.decrypt(updated_token.encrypted_access_token)).to eq('new_access_token')
+        expect(SuperOffice::Encryptor.decrypt(updated_token.encrypted_refresh_token)).to eq('refresh_token')
+      end
+    end
+
+    context 'unsuccessful refresh' do
+      it 'raises an error', :vcr do
+        expect { described_class.new(token).refresh }.to raise_error SuperOffice::OAuth::Token::Refresher::RefreshError
+      end
+    end
+  end
+
+  describe 'self.should_refresh?' do
+    context 'old token' do
+      it 'returns true' do
+        expect(described_class.should_refresh?(token)).to eq(true)
+      end
+    end
+
+    context 'new token' do
+      let(:token) { OpenStruct.new(expires_at: Date.parse('2050-01-01').to_time) }
+
+      it 'returns false' do
+        expect(described_class.should_refresh?(token)).to eq(false)
+      end
+    end
+  end
+end
